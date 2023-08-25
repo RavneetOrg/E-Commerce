dfdaf88a17371cd09d90ebe35589cfcc8d89ef75
Tarun <tarun.dogra@antiersolutions.com>
2023-08-25 12:07:42 +0530

    Please merge in master

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,13 @@ function Main() {
   return (
     <>
       <div className="hero border-1 pb-1" style={{height: "80vh"}}>
-        {/* <div className="card bg-dark text-white border-0 mx-3">
+        <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
             src="./assets/main.png.jpg"
             alt="Card"
-          /> */}
-          <div className="card-img-overlay d-flex align-items-center">
+          />
+          {/* <div className="card-img-overlay d-flex align-items-center"> */}
             <div className="container">
               <h5 className="card-title fs-1 text fw-lighter">
                 New Season Arrivals Please see
